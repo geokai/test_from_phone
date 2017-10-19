93575d7de55d46b2b447e04d7c8363429a012df9
George Kaimakis <geomatus@mac.com>
2017-10-19 17:07:18 +0100

    Initial Commit:
    	new file:   hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+"""my first android python script"""
+
+def greet_world():
+    print()
+    print('-' * 12)
+    print('Hello World!')
+    print('-' * 12)
+    print()
+
+if __name__ == '__main__':
+    greet_world()
